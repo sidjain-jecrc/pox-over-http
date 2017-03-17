17e03728375633dd767c66fb7d882428b5695d5d
Siddharth Jain <siddharthjain.jecrc@gmail.com>
2017-03-16 17:09:58 -0700

    +completed item exists logic in add item method

--- FoodMenuServer/src/main/java/com/asu/sid/foodmenuserver/FoodResourceServer.java
+++ FoodMenuServer/src/main/java/com/asu/sid/foodmenuserver/FoodResourceServer.java
@@ -5,9 +5,10 @@
  */
 package com.asu.sid.foodmenuserver;
 
-import com.asu.sid.beans.FoodItemData;
 import java.io.File;
 import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -15,10 +16,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -35,6 +41,7 @@ public class FoodResourceServer {
     private static final String ADD_FOOD = "NewFoodItems";
     private static final String GET_FOOD = "SelectedFoodItems";
     private static final String XML_NAMESPACE = "http://cse564.asu.edu/PoxAssignment";
+    private static String responseXmlString = null;
 
     public FoodResourceServer() {
         LOG.info("Creating a FoodResource Resource");
@@ -47,11 +54,8 @@ public class FoodResourceServer {
         LOG.info("Adding food item");
         LOG.info("Request Content = {}" + foodItem);
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = null;
-        String responseXML = null;
         try {
-            builder = factory.newDocumentBuilder();
+            DocumentBuilder builder = getDocBuilderInstance();
             Document document = builder.parse(new InputSource(new StringReader(foodItem)));
             document.getDocumentElement().normalize();
             String root = document.getDocumentElement().getNodeName();
@@ -60,29 +64,53 @@ public class FoodResourceServer {
             LOG.info("Root attribute: " + xmlNamespace);
 
             if (root == null || !root.equals(ADD_FOOD) || !root.equals(GET_FOOD) || xmlNamespace == null || !XML_NAMESPACE.equalsIgnoreCase(XML_NAMESPACE)) {
-                // return error xml
+                responseXmlString = "<InvalidMessage xmlns=”http://cse564.asu.edu/PoxAssignment”/>";
             }
 
             if (root.equals(ADD_FOOD)) {
                 NodeList foodList = document.getElementsByTagName("FoodItem");
                 for (int i = 0; i < foodList.getLength(); i++) {
                     Element foodElement = (Element) foodList.item(i);
-                    String foodName = foodElement.getElementsByTagName("name").item(0).getTextContent();
-                    String foodCategory = foodElement.getElementsByTagName("category").item(0).getTextContent();
-                    LOG.info("Food name: " + foodName + " and Category: " + foodCategory);
+                    String reqFoodName = foodElement.getElementsByTagName("name").item(0).getTextContent();
+                    String reqFoodCategory = foodElement.getElementsByTagName("category").item(0).getTextContent();
+                    LOG.info("Food name: " + reqFoodName + " and Category: " + reqFoodCategory);
 
                     // parse the stored xml file to check if the food item is already added or whether it exists
                     ClassLoader classLoader = getClass().getClassLoader();
                     File foodXmlFile = new File(classLoader.getResource("xml/FoodItemData.xml").getFile());
 
-                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    DocumentBuilder dBuilder = getDocBuilderInstance();
                     Document doc = dBuilder.parse(foodXmlFile);
 
-//                    JAXBContext jaxbContext = JAXBContext.newInstance(FoodItemData.class);
-//                    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-//                    FoodItemData foodItemData = (FoodItemData) jaxbUnmarshaller.unmarshal(foodXmlFile);
-//                    LOG.info("" + foodItemData);
+                    LOG.info("Root element :" + doc.getDocumentElement().getNodeName());
+                    NodeList foodItemList = doc.getElementsByTagName("FoodItem");
+
+                    for (int index = 0; index < foodItemList.getLength(); index++) {
+                        Element foodItemElement = (Element) foodItemList.item(index);
+                        String itemId = foodItemElement.getElementsByTagName("id").item(0).getTextContent();
+                        String localFoodName = foodItemElement.getElementsByTagName("name").item(0).getTextContent();
+                        String localFoodCategory = foodItemElement.getElementsByTagName("category").item(0).getTextContent();
+
+                        if (reqFoodName.equalsIgnoreCase(localFoodName) && reqFoodCategory.equalsIgnoreCase(localFoodCategory)) {
+                            // create an FoodItemExists xml
+                            DocumentBuilder existBuilder = getDocBuilderInstance();
+                            Document foodExistsDoc = existBuilder.newDocument();
+
+                            Element itemExistsElement = foodExistsDoc.createElement("FoodItemExists");
+                            itemExistsElement.setAttribute("xmlns", "http://cse564.asu.edu/PoxAssignment");
+                            foodExistsDoc.appendChild(itemExistsElement);
+
+                            Element idElement = foodExistsDoc.createElement("FoodItemId");
+                            idElement.appendChild(foodExistsDoc.createTextNode(itemId));
+                            itemExistsElement.appendChild(idElement);
+
+                            responseXmlString = getStringOfXmlDoc(foodExistsDoc);
+                        
+                        }else{
+                            // add new food item into the ezisting xml
+                        }
+                    }
+
                 }
             } else if (root.equals(GET_FOOD)) {
 
@@ -93,9 +121,35 @@ public class FoodResourceServer {
         }
 
         Response response;
-        response = Response.status(Response.Status.OK).entity(responseXML).build();
+        response = Response.status(Response.Status.OK).entity(responseXmlString).build();
 
         return response;
+    }
+
+    private DocumentBuilder getDocBuilderInstance() {
+        DocumentBuilder builder = null;
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            builder = docFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(FoodResourceServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return builder;
+    }
+
+    private String getStringOfXmlDoc(Document foodDoc) {
+        String xmlString = null;
+        try {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(foodDoc), new StreamResult(writer));
+            xmlString = writer.getBuffer().toString().replaceAll("\n|\r", "");
+        } catch (TransformerException ex) {
+            Logger.getLogger(FoodResourceServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return xmlString;
     }
 
 }
